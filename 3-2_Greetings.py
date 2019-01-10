8387cc71803f5923f427a210df1f66be909de450
Jairogn88 <44667756+Jairogn88@users.noreply.github.com>
2019-01-09 23:17:20 -0800

    Complete 3-2_Names

--- 3-2_Greetings.py
+++ 3-2_Greetings.py
@@ -2,3 +2,11 @@
 # printing each person’s name, print a message to them. The text of each mes-
 # sage should be the same, but each message should be personalized with the
 # person’s name.
+
+# create list names
+names = ['jairo', 'morty', 'rick']
+
+# # printing each item in a list
+print("Looking good " + names[0].title() + "!")
+print("Wubba lubba dub dub! " + names[1].title())
+print("Aw gee " + names[2].title() + "! I don't know about this!")
