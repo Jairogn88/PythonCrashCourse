e78c4899467055293264184d4f3cdff023b5e362
tiger-milk <jperez926@gmail.com>
2019-01-09 23:02:06 -0800

    Update 3-2_Greetings.py
    printed messages for friends

--- 3-2_Greetings.py
+++ 3-2_Greetings.py
@@ -2,3 +2,23 @@
 # printing each person’s name, print a message to them. The text of each mes-
 # sage should be the same, but each message should be personalized with the
 # person’s name.
+
+# Start with the list you used in Exercise 3-1, but instead of just
+# printing each person’s name, print a message to them. The text of each mes-
+# sage should be the same, but each message should be personalized with the
+# person’s name.
+
+#friendo list#
+names_list= ['bran', 'ned', 'john', 'nobody']
+
+#messages#
+message=('Hello, ' + names_list[0].title() + ' How you doin you beautiful bastard?')
+message1=('Hello, ' + names_list[1].title() + ' How you doin you beautiful bastard?')
+message2=('Hello, ' + names_list[2].title() + ' How you doin you beautiful bastard?')
+message3=('Hello, ' + names_list[3].title() + ' How you doin you beautiful bastard?')
+
+#printed messages#
+print(message)
+print(message1)
+print(message2)
+print(message3)
