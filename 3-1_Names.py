4268c0019b3c8b6de611aad84533677e61ceca03
tiger-milk <jperez926@gmail.com>
2019-01-09 22:33:06 -0800

    Update 3-1_Names.py
    added friends list. printed names

--- 3-1_Names.py
+++ 3-1_Names.py
@@ -1,3 +1,14 @@
 # Store the names of a few of your friends in a list called names . Print   #
 # each person’s name by accessing each element in the list, one at a time   #
 
+# Store the names of a few of your friends in a list called names . Print   #
+# each person’s name by accessing each element in the list, one at a time   #
+
+#friendo list#
+names_list= ['bran', 'ned', 'john', 'nobody']
+
+#friendos printed#
+print(names_list[0].title())
+print(names_list[1].title())
+print(names_list[2].title())
+print(names_list[3].title())
