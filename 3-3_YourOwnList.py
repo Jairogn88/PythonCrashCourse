ac86047e0ec92b13008ce16d5eaf25be37be1750
Jairogn88 <44667756+Jairogn88@users.noreply.github.com>
2019-01-09 23:45:16 -0800

    Create 3-3_YourOwnList.py

---
+++ 3-3_YourOwnList.py
@@ -0,0 +1,12 @@
+# |Think of your favorite mode of transportation, such as a                          |#
+# |motorcycle or a car, and make a list that stores several examples. Use your list  |#
+# |to print a series of statements about these items, such as “I would like to own a |#
+# |Honda motorcycle.”                                                                |#
+
+# create list of transport
+transport = ['motorcycle', 'bus', 'jetpack']
+
+# use each item in the list in a sentence
+print("I love the thrill riding a " + transport[0] + "!")
+print("Listening to music on the " + transport[1] + " is relaxing!")
+print("My " + transport[2] + " is my favorite form of transportation.")
