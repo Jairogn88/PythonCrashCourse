3b4e47dbee23d6084769594f3505076bc4a939a1
Jairogn88 <44667756+Jairogn88@users.noreply.github.com>
2019-01-09 22:52:35 -0800

    Add title case

--- 3-1_Names.py
+++ 3-1_Names.py
@@ -1,10 +1,12 @@
-# Store the names of a few of your friends in a list called names . Print   #
-# each person’s name by accessing each element in the list, one at a time   #
+# Start with the list you used in Exercise 3-1, but instead of just           #
+# printing each person’s name, print a message to them. The text of each mes- #
+# sage should be the same, but each message should be personalized with the   #
+# person’s name.                                                              #
 
 # create list names
 names = ['jairo', 'morty', 'rick']
 
 # # printing each item in a list
-print(names[0])
-print(names[1])
-print(names[2])
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
