9c8e3c68a9e9972e0419aa6adb52109abd368784
Jairogn88 <44667756+Jairogn88@users.noreply.github.com>
2019-01-09 22:17:18 -0800

    complete tiy 3.1

--- 3-1_Names.py
+++ 3-1_Names.py
@@ -1,3 +1,10 @@
 # Store the names of a few of your friends in a list called names . Print   #
 # each person’s name by accessing each element in the list, one at a time   #
 
+# create list names
+names = ['jairo', 'morty', 'rick']
+
+# # printing each item in a list
+print(names[0])
+print(names[1])
+print(names[2])
