0d5c626678566ea410fbd1d5f19036e96ab85119
tiger-milk <jperez926@gmail.com>
2019-01-10 21:21:36 -0800

    Update 3-3_YourOwnList.py
    list of cars

--- 3-3_YourOwnList.py
+++ 3-3_YourOwnList.py
@@ -2,3 +2,18 @@
 # |motorcycle or a car, and make a list that stores several examples. Use your list  |#
 # |to print a series of statements about these items, such as “I would like to own a |#
 # |Honda motorcycle.”                                                                |#
+
+#cars list#
+cars_list= ['honda', 'subaru', 'daewoo', 'toyota']
+
+#cars messages#
+message='i would like to own a ' + cars_list[0].title() + ' car'
+message1='i would like to own a ' + cars_list[1].title() + ' car'
+message2='i would like to own a ' + cars_list[2].title() + ' car'
+message3='i would like to own a ' + cars_list[3].title() + ' car'
+
+#printed messages#
+print(message)
+print(message1)
+print(message2)
+print(message3)
