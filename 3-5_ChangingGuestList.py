12663e3054587e7bd4eabd740007807f69015014
Jairogn88 <44667756+Jairogn88@users.noreply.github.com>
2019-01-23 14:34:53 -0800

    Create 3-5_ChangingGuestList.py

---
+++ 3-5_ChangingGuestList.py
@@ -0,0 +1,6 @@
+# # Oh no! One person isn't gonna be able to make it to the dinner party!
+# Start with  3-4_GuestList.py print a message at the end stating the name who can't make it to dinner
+
+# Replace the name of the guest who can't make it with the name of the new person you are inviting.
+
+# Print a second set of invitation messages, one for each person who is still in your list.
