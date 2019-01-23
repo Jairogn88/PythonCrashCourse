e2e8b885ab9b00f0ba6f22c6ec3cc187a283a9bf
Jairogn88 <44667756+Jairogn88@users.noreply.github.com>
2019-01-23 14:43:24 -0800

    Create 3-6_MoreGuests.py

---
+++ 3-6_MoreGuests.py
@@ -0,0 +1,10 @@
+# # You just found a bigger dinner table. Now you have more space avialable. Think of three more poeple.
+# Start with 3-5_MoreGuests.py, and add a print statement at the end informing people ou have found more space.
+
+# Use insert() to add a guest to the beginning of the guest list.
+
+# Use insert() to add a guest to the middle of the guest list.
+
+# Use append() to add a guest to the end of the guest list.
+
+# Print a new set of invitation messages, one for each person in your list.
