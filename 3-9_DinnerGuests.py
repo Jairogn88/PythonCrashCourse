8c718f21f5aecf7b6acf2e8580299358c6a4eeab
Jairogn88 <44667756+Jairogn88@users.noreply.github.com>
2019-01-23 14:56:07 -0800

    Create 3-9_DinnerGuests.py

---
+++ 3-9_DinnerGuests.py
@@ -0,0 +1,3 @@
+# Working with 3-7_ShrinkingGuestList.py, and use len() to print a message indicating the number
+# of people you are inviting to dinner. Try it througout different parts of the program.
+
