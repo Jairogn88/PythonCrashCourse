9ed1d8eecf1740378d6c16651a03c22eadc0ca7f
Jairogn88 <44667756+Jairogn88@users.noreply.github.com>
2019-01-23 18:10:24 -0800

    Update 3-4_GuestList.py

--- 3-4_GuestList.py
+++ 3-4_GuestList.py
@@ -1,3 +1,7 @@
 # Make of atleast three people you'd like to invite to dinner.
+invited = ['leo', 'dio', 'bruce']
 
 # Use the list to porint a message to each person inviting them to dinner.
+print("Hey " + invited[0] + ", you are invited to dinner.")
+print("Greetings " + invited[1] + ", you are invited to dinner.")
+print("Sup " + invited[2] + ", you are invited to dinner at my place.")
